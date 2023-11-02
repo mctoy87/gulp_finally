27b3d889792d73debab08a8fad1ac6de20b857b6
mctoy87 <mctoy87@mail.ru>
2023-11-02 15:57:03 +0200

    started get productInfo by js

--- src/js/modules/page.js
+++ src/js/modules/page.js
@@ -1,12 +1,15 @@
 // Отслеживаем клие на странице
 window.addEventListener('click', (event) => {
-
   // Проверяем что Клик был совершен по кнопке "Добавить в корзину"
   if (event.target.hasAttribute('data-cart')) {
-
     // находим карточку товара внутри которой был совершен Клик
-    const card = event.target.closest('.page-hero__price-wrap');
+    const card = event.target.closest('.page-hero__item');
     console.log(card);
-
   }
+  // Собираем данные с этого товара и записываем их в единый объект productInfo
+  const productInfo = {
+    id: card.dataset.id,
+    imgSrc: card.querySelector('.page-hero__img').getAttribute('src'),
+    title:  card.querySelector('.page-hero__title').innerText,
+  };
 });
